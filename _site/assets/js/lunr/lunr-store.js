d0a2f6b81b1e25ce8c453955d2b5651f8ebed95e
Dan Skinner <un25156@bristol.ac.uk>
2026-06-24 16:16:02 +0100

    update publications

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,19 +1,19 @@
 var store = [{
         "title": "New paper in the Quarterly Journal of the Royal Meteorological Society",
-        "excerpt":"In this new paper published in the Quarterly Journal of the Royal Meteorological Society, we show the effects of different ocean states on the South Pacific Convergence Zone.   Click below to read the paper in QJRMS.   Read the paper!  ","categories": ["new","paper"],
-        "tags": [],
-        "url": "/new/paper/2020/01/01/YYYY-MM-DD-QJ_paper_PROMS.html",
+        "excerpt":"In this new paper published in the Quarterly Journal of the Royal Meteorological Society, we show the effects of different ocean states on the South Pacific Convergence Zone.   Click below to read the paper in QJRMS.   Read the paper!  ","categories": [],
+        "tags": ["Papers"],
+        "url": "/2020/01/01/YYYY-MM-DD-QJ_paper_PROMS.html",
         "teaser": null
       },{
         "title": "New paper in the JOURNAL",
-        "excerpt":"In this new paper published in JOURNAL, we show   Click below to read the paper in JOURNAL.   Read the paper!  ","categories": ["new","paper"],
-        "tags": [],
-        "url": "/new/paper/2024/06/19/YYYY-MM-DD-NEW_PAPER_TEMPLATE.html",
+        "excerpt":"In this new paper published in JOURNAL, we show   Click below to read the paper in JOURNAL.   Read the paper!  ","categories": [],
+        "tags": ["Papers"],
+        "url": "/2024/06/19/YYYY-MM-DD-NEW_PAPER_TEMPLATE.html",
         "teaser": null
       },{
         "title": "Welcome to my new site!",
-        "excerpt":"The time had come for a slight restructure of my site, with a clear out of old content and a revamp of the layout! I am therefore very pleased to welcome you to DrDanSkinner.co.uk. As part of this restructuring I have added a News page, which will feature blog-style posts...","categories": ["website","update"],
-        "tags": [],
-        "url": "/website/update/2026/06/19/welcome-my-site.html",
+        "excerpt":"The time had come for a slight restructure of my site, with a clear out of old content and a revamp of the layout! I am therefore very pleased to welcome you to DrDanSkinner.co.uk. As part of this restructuring I have added a News page, which will feature blog-style posts...","categories": [],
+        "tags": ["Misc"],
+        "url": "/2026/06/19/welcome-my-site.html",
         "teaser": null
       }]

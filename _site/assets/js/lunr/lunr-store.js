78d3b015e8309275e4b628dcaaa6cbf463e4d097
Dan Skinner <un25156@bristol.ac.uk>
2026-09-03 11:09:10 +0100

    ipdated image settings

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Welcome to my new site!",
-        "excerpt":"The time had come for a slight restructure of my site, with a clear out of old content and a revamp of the layout! I am therefore very pleased to welcome you to DrDanSkinner.co.uk. As part of this restructuring I have added a News page, which will feature blog-style posts...","categories": [],
-        "tags": ["Misc"],
-        "url": "/2026/06/19/welcome-my-site.html",
+        "excerpt":"The time had come for a slight restructure of my site, with a clear out of old content and a revamp of the layout! I am therefore very pleased to welcome you to DrDanSkinner.co.uk. As part of this restructuring I have added a News page, which will feature blog-style posts...","categories": ["Events"],
+        "tags": [],
+        "url": "/events/2026/06/19/welcome-my-site.html",
+        "teaser": null
+      },{
+        "title": "New paper in QJRMS!",
+        "excerpt":"In this new paper published in the Quarterly Journal of the Royal Meteorological Society, we show the effects of different ocean states on the South Pacific Convergence Zone.   MORE INFO COMING SOON!   Click below to read the paper in QJRMS.   Read the paper!  ","categories": ["Papers"],
+        "tags": [],
+        "url": "/papers/2026/09/02/QJ_paper_PROMS.html",
         "teaser": null
       }]
